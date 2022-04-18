5b8e8d889759241f3cfb691d74a40638ccef20d8
J Brodie Day <jbrodieday@gmail.com>
2022-04-18 00:02:40 -0400

    Add tutorial notes file.

---
+++ tutorial/app.js
@@ -0,0 +1,16 @@
+let noz = 'noz'
+
+let zy = 'zy'
+
+let tozzy = 'tozzy'
+
+console.log(noz + zy + ` ${tozzy}`)
+
+let nozzytozzy = noz + zy + `  ${tozzy}`
+
+console.log(typeof null)
+
+// ** Reminder
+// Primitive Data Types: String, Number, Boolean, Null, Undefined, Symbol
+// Objects: Array, Function, Object
+  // {typeof null} will get reported as an object because of a quirk from original JS, but it's not really an object. 
